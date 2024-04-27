50c15cd012fda52fbcba2bb1db63c7bdd95279fe
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2024-04-27 15:37:29 -0700

    Renames fixture data to proper audio filenames

--- find_and_transcode_files_test.go
+++ find_and_transcode_files_test.go
@@ -22,28 +22,34 @@ func TestGetExclusiveFiles(t *testing.T) {
 		{
 			Name:            "Empty source list, destination list has elements",
 			SourceList:      []string{},
-			DestinationList: []string{"file1.txt", "file2.txt"},
+			DestinationList: []string{"file1.mp3", "file2.mp3"},
 			ExpectedOutput:  []string{},
 		},
 		{
 			Name:            "Source list has elements, empty destination list",
-			SourceList:      []string{"file1.txt", "file2.txt"},
+			SourceList:      []string{"file1.m4a", "file2.m4a"},
 			DestinationList: []string{},
-			ExpectedOutput:  []string{"file1.txt", "file2.txt"},
+			ExpectedOutput:  []string{"file1.mp3", "file2.mp3"},
 		},
 		{
 			Name:            "Source and destination have common elements",
-			SourceList:      []string{"file1.txt", "file2.txt", "file3.txt"},
-			DestinationList: []string{"file2.txt", "file3.txt", "file4.txt"},
-			ExpectedOutput:  []string{"file1.txt"},
+			SourceList:      []string{"file1.m4a", "file2.m4a", "file3.m4a"},
+			DestinationList: []string{"file2.mp3", "file3.mp3", "file4.mp3"},
+			ExpectedOutput:  []string{"file1.mp3"},
 		},
 		{
 			Name:            "Source and destination have disjoint elements",
-			SourceList:      []string{"file1.txt", "file2.txt", "file3.txt"},
-			DestinationList: []string{"file4.txt", "file5.txt", "file6.txt"},
-			ExpectedOutput:  []string{"file1.txt", "file2.txt", "file3.txt"},
+			SourceList:      []string{"file1.m4a", "file2.m4a", "file3.m4a"},
+			DestinationList: []string{"file4.mp3", "file5.mp3", "file6.mp3"},
+			ExpectedOutput:  []string{"file1.mp3", "file2.mp3", "file3.mp3"},
 		},
 	}
+
+	// Test case: mp3 files as source
+
+	// Test case: both mp3 and m4a files as source
+
+	// Test case: m4a files in destination
 
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {
