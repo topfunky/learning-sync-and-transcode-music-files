22ab69e63c6af26294bb56f81c132247ebba05c9
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2024-05-03 22:21:59 -0700

    Refactors `main.go` to use `flag` package
    Uses named options instead of indexed args

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"flag"
 )
 
-// TODO: Use named options as arg to command line for source and destination
 func main() {
-	if len(os.Args) > 2 {
-		sourceDir := os.Args[1]
-		destinationDir := os.Args[2]
-		findFiles(sourceDir, destinationDir)
-	} else {
-		fmt.Println("Usage: sync_and_transcode_music_files <source-directory> <destination-directory>")
-		os.Exit(1)
-	}
+	sourcePtr := flag.String("source", "source", "Directory in which to find original music files")
+	destinationPtr := flag.String("destination", "destination", "Output directory for transcoded files")
+
+	flag.Parse()
+
+	sourceDir := *sourcePtr
+	destinationDir := *destinationPtr
+	findFiles(sourceDir, destinationDir)
 }
