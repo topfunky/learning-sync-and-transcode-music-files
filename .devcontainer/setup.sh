3b126047f9b2bbbb6e9fc8d982e5504cbdb368e1
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2024-05-24 05:47:52 +0000

    Updates setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-apt-get install -y curl
+git config --global --add safe.directory /workspaces/learning-sync-and-transcode-music-files
+
+apt-get update
+apt-get install -y build-essential procps file git gcc
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
-
-sudo apt-get install build-essential procps file git
 
 brew install gum ffmpeg jq
 
