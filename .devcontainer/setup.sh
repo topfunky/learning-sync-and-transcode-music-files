81e117b7ec1a9f148adaf6eba18a8d70038ed84e
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2024-05-24 05:30:10 +0000

    Removes redundant installation of curl with apt-get

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,7 +8,7 @@ test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 
-sudo apt-get install build-essential procps curl file git
+sudo apt-get install build-essential procps file git
 
 brew install gum ffmpeg jq
 
