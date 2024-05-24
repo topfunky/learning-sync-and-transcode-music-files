4904496df44c69e6b3bf896bde0303f7b37cc7bb
Geoffrey Grosenbach <26+topfunky@users.noreply.github.com>
2024-05-24 05:13:09 +0000

    Adds install script

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
+
+sudo apt-get install build-essential procps curl file git
+
+brew install gum ffmpeg
+
